4658e92b6690ea06dbd8d4db203cebee0a17d3bf
Igor Šarčević <igor@renderedtext.com>
2019-02-26 18:20:24 +0100

    Bootstrap docker compose executor

--- pkg/api/job_request.go
+++ pkg/api/job_request.go
@@ -8,6 +8,19 @@ import (
 	yaml "gopkg.in/yaml.v2"
 )
 
+type Container struct {
+	Name    string   `json:"name" yaml:"name"`
+	Image   string   `json:"image" yaml:"image"`
+	Command string   `json:"command" yaml:"command"`
+	EnvVars []EnvVar `json:"env_vars" yaml:"env_vars"`
+	Files   []File   `json:"files" yaml:"file"`
+}
+
+type Compose struct {
+	Creadentilals File        `json:"credentials" yaml:"credentials"`
+	Containers    []Container `json:"containers" yaml:"containers"`
+}
+
 type Command struct {
 	Directive string `json:"directive" yaml:"directive"`
 }
@@ -30,6 +43,7 @@ type Callbacks struct {
 }
 
 type JobRequest struct {
+	Compose          Compose   `json:"compose" yaml:"compose"`
 	Commands         []Command `json:"commands" yaml:"commands"`
 	EpilogueCommands []Command `json:"epilogue_commands" yaml:"epilogue_commands"`
 	EnvVars          []EnvVar  `json:"env_vars" yaml:"env_vars"`
