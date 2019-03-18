34aeda938da044889c7e1c7bc89cfa13cfe85948
Igor Šarčević <igor@renderedtext.com>
2019-03-18 16:20:00 +0000

    Inject docker pull logs into e2e tests

--- test/e2e/docker/file_injection_broken_file_mode.rb
+++ test/e2e/docker/file_injection_broken_file_mode.rb
@@ -45,6 +45,11 @@ wait_for_job_to_finish
 assert_job_log <<-LOG
   {"event":"job_started",  "timestamp":"*"}
 
+  {"event":"cmd_started",  "timestamp":"*", "directive":"Pulling docker images..."}
+  {"event":"cmd_output",   "timestamp":"*", "output":"*"}
+  {"event":"cmd_output",   "timestamp":"*", "output":"*"}
+  {"event":"cmd_finished", "timestamp":"*", "directive":"Pulling docker images...","event":"cmd_finished","exit_code":0,"finished_at":"*","started_at":"*","timestamp":"*"}
+
   {"event":"cmd_started",  "timestamp":"*", "directive":"Exporting environment variables"}
   {"event":"cmd_finished", "timestamp":"*", "directive":"Exporting environment variables","exit_code":0,"finished_at":"*","started_at":"*"}
 
