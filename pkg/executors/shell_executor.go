8887d516c0e75dfb643f5518f9640c079b7e4258
Igor Šarčević <igor@renderedtext.com>
2019-09-27 15:34:53 +0200

    Extract command output handler

--- pkg/executors/shell_executor.go
+++ pkg/executors/shell_executor.go
@@ -10,7 +10,6 @@ import (
 	"os/exec"
 	"path"
 	"regexp"
-	"strconv"
 	"strings"
 	"time"
 
@@ -332,62 +331,9 @@ func (e *ShellExecutor) RunCommand(command string, callback EventHandler) int {
 
 	e.stdin.Write([]byte(commandWithStartAndEndMarkers))
 
-	log.Println("Scan started")
+	reader = NewCommandOutputReader(callback)
 
-	err = ScanLines(e.tty, func(line string) bool {
-		log.Printf("(tty) %s\n", line)
-
-		if strings.Contains(line, startMark) {
-			log.Printf("Detected command start")
-			streamEvents = true
-
-			callback(NewCommandStartedEvent(command))
-
-			return true
-		}
-
-		if strings.Contains(line, finishMark) {
-			log.Printf("Detected command end")
-
-			finalOutputPart := strings.Split(line, finishMark)
-
-			// if there is anything else other than the command end marker
-			// print it to the user
-			if finalOutputPart[0] != "" {
-				callback(NewCommandOutputEvent(finalOutputPart[0] + "\n"))
-			}
-
-			streamEvents = false
-
-			if match := commandEndRegex.FindStringSubmatch(line); len(match) == 2 {
-				log.Printf("Parsing exit status succedded")
-
-				exitCode, err = strconv.Atoi(match[1])
-
-				if err != nil {
-					log.Printf("Panic while parsing exit status, err: %+v", err)
-
-					callback(NewCommandOutputEvent("Failed to read command exit code\n"))
-				}
-
-				log.Printf("Setting exit code to %d", exitCode)
-			} else {
-				log.Printf("Failed to parse exit status")
-
-				exitCode = 1
-				callback(NewCommandOutputEvent("Failed to read command exit code\n"))
-			}
-
-			log.Printf("Stopping scanner")
-			return false
-		}
-
-		if streamEvents {
-			callback(NewCommandOutputEvent(line + "\n"))
-		}
-
-		return true
-	})
+	err = reader.Scan(callback)
 
 	return exitCode
 }
