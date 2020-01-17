48d8fd465a921706562fcc3a4c62c2d107425544
Igor Šarčević <igor@renderedtext.com>
2020-01-17 14:28:43 +0100

    Green tests for Docker x Unicode support

--- test/e2e/docker/unicode.rb
+++ test/e2e/docker/unicode.rb
@@ -1,4 +1,7 @@
 #!/bin/ruby
+
+Encoding.default_external = Encoding::UTF_8
+
 # rubocop:disable all
 
 require_relative '../../e2e'
@@ -52,8 +55,12 @@ assert_job_log <<-LOG
   {"event":"cmd_started",  "timestamp":"*", "directive":"Injecting Files"}
   {"event":"cmd_finished", "timestamp":"*", "directive":"Injecting Files","exit_code":0,"finished_at":"*","started_at":"*"}
   {"event":"cmd_started",  "timestamp":"*", "directive":"echo 特定の伝説に拠る物語の由来については諸説存在し。特定の伝説に拠る物語の由来については諸説存在し。特定の伝説に拠る物語の由来については諸説存在し。"}
-  {"event":"cmd_output",   "timestamp":"*", "output":"特定の伝説に拠る物語の由来については諸説存在し。特定の伝説に拠る物語の由来については諸説存在し。特定の伝説に拠る物語の由来については諸説存在し。\\n"}
-  {"event":"cmd_finished", "timestamp":"*", "directive":"echo 特定の伝説に拠る物語の由来については諸説存在し。","exit_code":0,"finished_at":"*","started_at":"*"}
+
+  {"event":"cmd_output",   "timestamp":"*", "output":"特定の伝説に拠る物語の由来については諸説存在し。特定の伝説に拠る"}
+  {"event":"cmd_output",   "timestamp":"*", "output":"物語の由来については諸説存在し。特定の伝説に拠る物語の由来につい"}
+  {"event":"cmd_output",   "timestamp":"*", "output":"ては諸説存在し。\\n"}
+
+  {"event":"cmd_finished", "timestamp":"*", "directive":"echo 特定の伝説に拠る物語の由来については諸説存在し。特定の伝説に拠る物語の由来については諸説存在し。特定の伝説に拠る物語の由来については諸説存在し。","exit_code":0,"finished_at":"*","started_at":"*"}
   {"event":"cmd_started",  "timestamp":"*", "directive":"export SEMAPHORE_JOB_RESULT=passed"}
   {"event":"cmd_finished", "timestamp":"*", "directive":"export SEMAPHORE_JOB_RESULT=passed","exit_code":0,"finished_at":"*","started_at":"*"}
   {"event":"job_finished", "timestamp":"*", "result":"passed"}
