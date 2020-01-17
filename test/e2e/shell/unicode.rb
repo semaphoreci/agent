58691a9a13bad325420406dcb3be39b06e36b0a5
Igor Šarčević <igor@renderedtext.com>
2020-01-17 14:37:37 +0100

    Green tests for Shell x Unicode

--- test/e2e/shell/unicode.rb
+++ test/e2e/shell/unicode.rb
@@ -10,19 +10,7 @@ start_job <<-JSON
   {
     "id": "#{$JOB_ID}",
 
-    "executor": "dockercompose",
-
-    "compose": {
-      "containers": [
-        {
-          "name": "main",
-          "image": "ruby:2.6"
-        }
-      ]
-    },
-
     "env_vars": [],
-
     "files": [],
 
     "commands": [
@@ -42,18 +30,12 @@ wait_for_job_to_finish
 
 assert_job_log <<-LOG
   {"event":"job_started",  "timestamp":"*"}
-  {"event":"cmd_started",  "timestamp":"*", "directive":"Pulling docker images..."}
-  *** LONG_OUTPUT ***
-  {"event":"cmd_finished", "timestamp":"*", "directive":"Pulling docker images...","event":"cmd_finished","exit_code":0,"finished_at":"*","started_at":"*","timestamp":"*"}
-
-  {"event":"cmd_started",  "timestamp":"*", "directive":"Starting the docker image..."}
-  {"event":"cmd_output",   "timestamp":"*", "output":"Starting a new bash session.\\n"}
-  {"event":"cmd_finished", "timestamp":"*", "directive":"Starting the docker image...","event":"cmd_finished","exit_code":0,"finished_at":"*","started_at":"*","timestamp":"*"}
 
   {"event":"cmd_started",  "timestamp":"*", "directive":"Exporting environment variables"}
   {"event":"cmd_finished", "timestamp":"*", "directive":"Exporting environment variables","exit_code":0,"finished_at":"*","started_at":"*"}
   {"event":"cmd_started",  "timestamp":"*", "directive":"Injecting Files"}
   {"event":"cmd_finished", "timestamp":"*", "directive":"Injecting Files","exit_code":0,"finished_at":"*","started_at":"*"}
+
   {"event":"cmd_started",  "timestamp":"*", "directive":"echo 特定の伝説に拠る物語の由来については諸説存在し。特定の伝説に拠る物語の由来については諸説存在し。特定の伝説に拠る物語の由来については諸説存在し。"}
 
   {"event":"cmd_output",   "timestamp":"*", "output":"特定の伝説に拠る物語の由来については諸説存在し。特定の伝説に拠る"}
@@ -61,7 +43,9 @@ assert_job_log <<-LOG
   {"event":"cmd_output",   "timestamp":"*", "output":"ては諸説存在し。\\n"}
 
   {"event":"cmd_finished", "timestamp":"*", "directive":"echo 特定の伝説に拠る物語の由来については諸説存在し。特定の伝説に拠る物語の由来については諸説存在し。特定の伝説に拠る物語の由来については諸説存在し。","exit_code":0,"finished_at":"*","started_at":"*"}
+
   {"event":"cmd_started",  "timestamp":"*", "directive":"export SEMAPHORE_JOB_RESULT=passed"}
   {"event":"cmd_finished", "timestamp":"*", "directive":"export SEMAPHORE_JOB_RESULT=passed","exit_code":0,"finished_at":"*","started_at":"*"}
+
   {"event":"job_finished", "timestamp":"*", "result":"passed"}
 LOG
