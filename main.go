a1d9e4479751a230cbb131e6e03b6690b5bed47d
Igor Šarčević <igor@renderedtext.com>
2020-02-10 11:28:35 +0100

    Run utf-8 replication excersice

--- main.go
+++ main.go
@@ -2,87 +2,13 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"log"
-	"math/rand"
-	"os"
-	"time"
 
-	api "github.com/semaphoreci/agent/pkg/api"
-	jobs "github.com/semaphoreci/agent/pkg/jobs"
-	server "github.com/semaphoreci/agent/pkg/server"
-	pflag "github.com/spf13/pflag"
+	petname "github.com/dustinkirkland/golang-petname"
 )
 
-var VERSION = "dev"
-
 func main() {
-	// Initialize global randomness
-	rand.Seed(time.Now().UnixNano())
-
-	action := os.Args[1]
-
-	logfile := OpenLogfile()
-	log.SetOutput(logfile)
-	log.SetFlags(log.Ldate | log.Lmicroseconds | log.Lshortfile)
-
-	switch action {
-	case "serve":
-		RunServer(logfile)
-	case "run":
-		RunSingleJob()
-	case "version":
-		fmt.Println(VERSION)
+	for i := 0; i < 10000; i++ {
+		name := petname.Generate(3, "-")
+		fmt.Printf("│││││││││││││ %30s │││││││││││││││\n", name)
 	}
-}
-
-func OpenLogfile() io.Writer {
-	f, err := os.OpenFile("/tmp/agent_log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return io.MultiWriter(f, os.Stdout)
-}
-
-func RunServer(logfile io.Writer) {
-	authTokenSecret := pflag.String("auth-token-secret", "", "Auth token for accessing the server")
-	port := pflag.Int("port", 8000, "Port of the server")
-	host := pflag.String("host", "0.0.0.0", "Host of the server")
-	tlsCertPath := pflag.String("tls-cert-path", "server.crt", "TLS Certificate path")
-	tlsKeyPath := pflag.String("tls-key-path", "server.key", "TLS Private key path")
-
-	pflag.Parse()
-
-	if *authTokenSecret == "" {
-		log.Fatal("Auth token is empty")
-	}
-
-	server.NewServer(
-		*host,
-		*port,
-		*tlsCertPath,
-		*tlsKeyPath,
-		VERSION,
-		logfile,
-		[]byte(*authTokenSecret),
-	).Serve()
-}
-
-func RunSingleJob() {
-	request, err := api.NewRequestFromYamlFile(os.Args[2])
-
-	if err != nil {
-		panic(err)
-	}
-
-	job, err := jobs.NewJob(request)
-	if err != nil {
-		panic(err)
-	}
-
-	job.JobLogArchived = true
-
-	job.Run()
 }
