fbf591fb3c3826f91812708ed8bf599f387d0ca2
Lucas Pinheiro <pinheiro.lucasaugusto@gmail.com>
2021-07-26 11:05:00 -0300

    Assert correct logs

--- test/e2e/docker/multiple_containers.rb
+++ test/e2e/docker/multiple_containers.rb
@@ -56,9 +56,9 @@ assert_job_log <<-LOG
   {"event":"cmd_finished", "timestamp":"*", "directive":"Exporting environment variables","exit_code":0,"finished_at":"*","started_at":"*"}
   {"event":"cmd_started",  "timestamp":"*", "directive":"Injecting Files"}
   {"event":"cmd_finished", "timestamp":"*", "directive":"Injecting Files","exit_code":0,"finished_at":"*","started_at":"*"}
-  {"event":"cmd_started",  "timestamp":"*", "directive":"docker ps -a | grep db | wc -l"}
+  {"event":"cmd_started",  "timestamp":"*", "directive":"docker ps -a | grep postgres | wc -l"}
   {"event":"cmd_output",   "timestamp":"*", "output":"1\\n"}
-  {"event":"cmd_finished", "timestamp":"*", "directive":"docker ps -a | grep db | wc -l","exit_code":0,"finished_at":"*","started_at":"*"}
+  {"event":"cmd_finished", "timestamp":"*", "directive":"docker ps -a | grep postgres | wc -l","exit_code":0,"finished_at":"*","started_at":"*"}
   {"event":"cmd_started",  "timestamp":"*", "directive":"export SEMAPHORE_JOB_RESULT=passed"}
   {"event":"cmd_finished", "timestamp":"*", "directive":"export SEMAPHORE_JOB_RESULT=passed","exit_code":0,"finished_at":"*","started_at":"*"}
   {"event":"job_finished", "timestamp":"*", "result":"passed"}
