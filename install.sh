a2da68d056715f4c83ba9a9cd8a30739585fff3e
Lucas Pinheiro <pinheiro.lucasaugusto@gmail.com>
2021-08-09 13:40:47 -0300

    Include install.sh script in release

--- install.sh
+++ install.sh
@@ -20,16 +20,7 @@ if [[ -z $registration_token ]]; then
   exit 1
 fi
 
-# TODO: allow version to be specified
-agent_version=latest
-
-# Other possible values are: Linux_i386 and Darwin_x86_64
-read -p "Enter architecture [Linux_x86_64]: " agent_os
-agent_os="${agent_os:=Linux_x86_64}"
-
-read -p "Enter installation directory [/opt/semaphore]: " install_directory
-install_directory="${install_directory:=/opt/semaphore}"
-
+install_directory=$(pwd)
 logged_in_user=$(logname)
 read -p "Enter user [$logged_in_user]: " install_user
 install_user="${install_user:=$logged_in_user}"
@@ -38,21 +29,6 @@ if ! id "$install_user" &>/dev/null; then
   echo "User $install_user does not exist. Exiting..."
   exit 1
 fi
-
-echo "Creating $install_directory directory..."
-sudo mkdir -p $install_directory
-sudo chown $install_user:$install_user $install_directory
-
-agent_url="https://github.com/semaphoreci/agent/releases/latest/download/agent_$agent_os.tar.gz"
-echo "Downloading $agent_url..."
-status_code=$(curl -w "%{http_code}" -sL $agent_url -o $install_directory/agent.tar.gz)
-if [[ $status_code -ne "200" ]]; then
-  echo "Error downloading agent: $status_code"
-  exit 1
-fi
-
-echo "Extracting $install_directory/agent.tar.gz..."
-tar -xf $install_directory/agent.tar.gz -C $install_directory
 
 AGENT_CONFIG=$(cat <<-END
 endpoint: "$organization.semaphoreci.com"
