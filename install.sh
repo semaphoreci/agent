a3d19e90b93f677aca475ea7ae3c8a04e4f4d337
Lucas Pinheiro <pinheiro.lucasaugusto@gmail.com>
2021-10-04 15:14:15 -0300

    Include toolbox installation (#126)

--- install.sh
+++ install.sh
@@ -3,36 +3,62 @@
 set -e
 set -o pipefail
 
+AGENT_INSTALLATION_DIRECTORY=$(pwd)
+LOGGED_IN_USER=$(logname)
+TOOLBOX_VERSION="v1.14.8"
+
 if [[ "$EUID" -ne 0 ]]; then
   echo "Please run with sudo."
   exit 1
 fi
 
-read -p "Enter organization: " organization
-if [[ -z $organization ]]; then
-  echo "Organization cannot be empty."
+if [[ -z $SEMAPHORE_ORGANIZATION ]]; then
+  read -p "Enter organization: " SEMAPHORE_ORGANIZATION
+  if [[ -z $SEMAPHORE_ORGANIZATION ]]; then
+    echo "Organization cannot be empty."
+    exit 1
+  fi
+fi
+
+if [[ -z $SEMAPHORE_REGISTRATION_TOKEN ]]; then
+  read -p "Enter registration token: " SEMAPHORE_REGISTRATION_TOKEN
+  if [[ -z $SEMAPHORE_REGISTRATION_TOKEN ]]; then
+    echo "Registration token cannot be empty."
+    exit 1
+  fi
+fi
+
+if [[ -z $SEMAPHORE_AGENT_INSTALLATION_USER ]]; then
+  read -p "Enter user [$LOGGED_IN_USER]: " SEMAPHORE_AGENT_INSTALLATION_USER
+  SEMAPHORE_AGENT_INSTALLATION_USER="${SEMAPHORE_AGENT_INSTALLATION_USER:=$LOGGED_IN_USER}"
+fi
+
+if ! id "$SEMAPHORE_AGENT_INSTALLATION_USER" &>/dev/null; then
+  echo "User $SEMAPHORE_AGENT_INSTALLATION_USER does not exist. Exiting..."
   exit 1
 fi
 
-read -p "Enter registration token: " registration_token
-if [[ -z $registration_token ]]; then
-  echo "Registration token cannot be empty."
-  exit 1
-fi
+#
+# Download toolbox
+#
+echo "Installing toolbox..."
+USER_HOME_DIRECTORY=$(sudo -u $SEMAPHORE_AGENT_INSTALLATION_USER -H bash -c 'echo $HOME')
+curl -sL "https://github.com/semaphoreci/toolbox/releases/download/$TOOLBOX_VERSION/self-hosted-linux.tar" -o toolbox.tar
+tar -xf toolbox.tar
 
-install_directory=$(pwd)
-logged_in_user=$(logname)
-read -p "Enter user [$logged_in_user]: " install_user
-install_user="${install_user:=$logged_in_user}"
+mv toolbox $USER_HOME_DIRECTORY/.toolbox
+sudo chown -R $SEMAPHORE_AGENT_INSTALLATION_USER:$SEMAPHORE_AGENT_INSTALLATION_USER $USER_HOME_DIRECTORY/.toolbox
 
-if ! id "$install_user" &>/dev/null; then
-  echo "User $install_user does not exist. Exiting..."
-  exit 1
-fi
+sudo -u $SEMAPHORE_AGENT_INSTALLATION_USER -H bash $USER_HOME_DIRECTORY/.toolbox/install-toolbox
+echo "source ~/.toolbox/toolbox" >> $USER_HOME_DIRECTORY/.bash_profile
+rm toolbox.tar
 
+#
+# Create agent config
+#
 AGENT_CONFIG=$(cat <<-END
-endpoint: "$organization.semaphoreci.com"
-token: "$registration_token"
+endpoint: "$SEMAPHORE_ORGANIZATION.semaphoreci.com"
+token: "$SEMAPHORE_REGISTRATION_TOKEN"
 no-https: false
 shutdown-hook-path: ""
 disconnect-after-job: false
@@ -42,11 +68,14 @@ fail-on-missing-files: false
 END
 )
 
-AGENT_CONFIG_PATH="$install_directory/config.yaml"
+AGENT_CONFIG_PATH="$AGENT_INSTALLATION_DIRECTORY/config.yaml"
 echo "Creating agent config file at $AGENT_CONFIG_PATH..."
 echo "$AGENT_CONFIG" > $AGENT_CONFIG_PATH
-sudo chown $install_user:$install_user $AGENT_CONFIG_PATH
+sudo chown $SEMAPHORE_AGENT_INSTALLATION_USER:$SEMAPHORE_AGENT_INSTALLATION_USER $AGENT_CONFIG_PATH
 
+#
+# Create systemd service
+#
 SYSTEMD_SERVICE=$(cat <<-END
 [Unit]
 Description=Semaphore agent
@@ -57,9 +86,9 @@ StartLimitIntervalSec=0
 Type=simple
 Restart=always
 RestartSec=5
-User=$install_user
-WorkingDirectory=$install_directory
-ExecStart=$install_directory/agent start --config-file $AGENT_CONFIG_PATH
+User=$SEMAPHORE_AGENT_INSTALLATION_USER
+WorkingDirectory=$AGENT_INSTALLATION_DIRECTORY
+ExecStart=$AGENT_INSTALLATION_DIRECTORY/agent start --config-file $AGENT_CONFIG_PATH
 
 [Install]
 WantedBy=multi-user.target
