f0c5a225929cccaa61dd131199c18b37988f7de5
Lucas Pinheiro <lpinheiro@renderedtext.com>
2022-07-08 17:25:09 -0300

    add test for expired token for httpbackend

--- test/support/loghub.go
+++ test/support/loghub.go
@@ -8,6 +8,8 @@ import (
 	"strings"
 )
 
+const ExpiredLogToken = "expired-token"
+
 type LoghubMockServer struct {
 	Logs    []string
 	Server  *httptest.Server
@@ -21,23 +23,46 @@ func NewLoghubMockServer() *LoghubMockServer {
 }
 
 func (m *LoghubMockServer) Init() {
-	mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodPost {
-			fmt.Println("[LOGHUB MOCK] Received logs")
-			body, err := ioutil.ReadAll(r.Body)
-			if err != nil {
-				fmt.Printf("Error reading body: %v\n", err)
-			}
-
-			logs := strings.Split(string(body), "\n")
-			m.Logs = append(m.Logs, FilterEmpty(logs)...)
-			w.WriteHeader(200)
-		} else {
-			fmt.Println("NOPE")
-		}
-	}))
-
+	mockServer := httptest.NewServer(http.HandlerFunc(m.handler))
 	m.Server = mockServer
+}
+
+func (m *LoghubMockServer) handler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	// just an easy way to mock the expired token scenario
+	token, err := m.findToken(r)
+	if err != nil || token == ExpiredLogToken {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	fmt.Println("[LOGHUB MOCK] Received logs")
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Printf("Error reading body: %v\n", err)
+	}
+
+	logs := strings.Split(string(body), "\n")
+	m.Logs = append(m.Logs, FilterEmpty(logs)...)
+	w.WriteHeader(200)
+}
+
+func (m *LoghubMockServer) findToken(r *http.Request) (string, error) {
+	reqToken := r.Header.Get("Authorization")
+	if reqToken == "" {
+		return "", fmt.Errorf("no token found")
+	}
+
+	splitToken := strings.Split(reqToken, "Bearer ")
+	if len(splitToken) != 2 {
+		return "", fmt.Errorf("malformed token")
+	}
+
+	return splitToken[1], nil
 }
 
 func (m *LoghubMockServer) GetLogs() []string {
