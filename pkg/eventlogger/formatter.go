4ec5920a6a8c4ddde4704bb171e5096d1521d297
Lucas Pinheiro <lpinheiro@renderedtext.com>
2022-08-11 17:46:12 -0300

    feat: always include agent name in logs

--- pkg/eventlogger/formatter.go
+++ pkg/eventlogger/formatter.go
@@ -2,15 +2,45 @@ package eventlogger
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
 	log "github.com/sirupsen/logrus"
 )
 
 type CustomFormatter struct {
+	AgentName string
 }
 
 func (f *CustomFormatter) Format(entry *log.Entry) ([]byte, error) {
-	log := fmt.Sprintf("%-20s: %s\n", entry.Time.UTC().Format(time.StampMilli), entry.Message)
+	extraFields := f.formatFields(entry.Data)
+	if extraFields == "" {
+		log := fmt.Sprintf(
+			"%s agent_name=%s : %s\n",
+			entry.Time.UTC().Format(time.StampMilli),
+			f.AgentName,
+			entry.Message,
+		)
+
+		return []byte(log), nil
+	}
+
+	log := fmt.Sprintf(
+		"%s agent_name=%s %s : %s\n",
+		entry.Time.UTC().Format(time.StampMilli),
+		f.AgentName,
+		extraFields,
+		entry.Message,
+	)
+
 	return []byte(log), nil
+}
+
+func (f *CustomFormatter) formatFields(fields log.Fields) string {
+	result := []string{}
+	for key, value := range fields {
+		result = append(result, fmt.Sprintf("%s=%s", key, value))
+	}
+
+	return strings.Join(result, " ")
 }
