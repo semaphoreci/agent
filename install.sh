d086a01cfefe2efb4bdf0cfb59a355c0f966434d
Lucas Pinheiro <lpinheiro@renderedtext.com>
2022-08-22 09:26:25 -0300

    feat: launchd support in installation script (#167)

--- install.sh
+++ install.sh
@@ -3,6 +3,130 @@
 set -e
 set -o pipefail
 
+#
+# Creates a 'semaphore-agent' systemd service.
+# If it already exists, it will be overriden.
+# SEMAPHORE_AGENT_START controls whether the service will be started as well.
+#
+create_systemd_service() {
+  SYSTEMD_SERVICE=$(cat <<-END
+[Unit]
+Description=Semaphore agent
+After=network.target
+StartLimitIntervalSec=0
+
+[Service]
+Type=simple
+Restart=$SEMAPHORE_AGENT_SYSTEMD_RESTART
+RestartSec=$SEMAPHORE_AGENT_SYSTEMD_RESTART_SEC
+User=$SEMAPHORE_AGENT_INSTALLATION_USER
+WorkingDirectory=$AGENT_INSTALLATION_DIRECTORY
+ExecStart=$AGENT_INSTALLATION_DIRECTORY/agent start --config-file $AGENT_CONFIG_PATH
+
+[Install]
+WantedBy=multi-user.target
+END
+  )
+
+  SYSTEMD_PATH=/etc/systemd/system
+  SERVICE_NAME=semaphore-agent
+  SYSTEMD_SERVICE_PATH=$SYSTEMD_PATH/$SERVICE_NAME.service
+
+  echo "Creating $SYSTEMD_SERVICE_PATH..."
+
+  if [[ -f "$SYSTEMD_SERVICE_PATH" ]]; then
+    echo "systemd service already exists at $SYSTEMD_SERVICE_PATH. Overriding it..."
+    echo "$SYSTEMD_SERVICE" > $SYSTEMD_SERVICE_PATH
+    systemctl daemon-reload
+    if [[ "$SEMAPHORE_AGENT_START" == "false" ]]; then
+      echo "Not restarting agent."
+    else
+      echo "Restarting semaphore-agent service..."
+      systemctl restart semaphore-agent
+    fi
+  else
+    echo "$SYSTEMD_SERVICE" > $SYSTEMD_SERVICE_PATH
+    if [[ "$SEMAPHORE_AGENT_START" == "false" ]]; then
+      echo "Not starting agent."
+    else
+      echo "Starting semaphore-agent service..."
+      systemctl start semaphore-agent
+    fi
+  fi
+}
+
+#
+# Creates a 'com.semaphoreci.agent' launchd daemon, at /Library/LaunchDaemons.
+# If it already exists, it will be overriden. SEMAPHORE_AGENT_START controls
+# whether the daemon will be started as well.
+#
+create_launchd_daemon() {
+  LAUNCHD_DAEMON_LABEL=com.semaphoreci.agent
+  LAUNCHD_DAEMON=$(cat <<-END
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+  <key>Label</key>
+  <string>$LAUNCHD_DAEMON_LABEL</string>
+  <key>ProgramArguments</key>
+  <array>
+    <string>$AGENT_INSTALLATION_DIRECTORY/agent</string>
+    <string>start</string>
+    <string>--config-file</string>
+    <string>$AGENT_CONFIG_PATH</string>
+  </array>
+  <key>RunAtLoad</key>
+  <true/>
+  <key>KeepAlive</key>
+  <dict>
+    <key>Crashed</key>
+    <true/>
+  </dict>
+  <key>UserName</key>
+  <string>$SEMAPHORE_AGENT_INSTALLATION_USER</string>
+  <key>WorkingDirectory</key>
+  <string>$AGENT_INSTALLATION_DIRECTORY</string>
+</dict>
+</plist>
+END
+  )
+
+  LAUNCHD_PATH=/Library/LaunchDaemons
+  LAUNCHD_DAEMON_PATH=$LAUNCHD_PATH/$LAUNCHD_DAEMON_LABEL.plist
+
+  echo "Creating $LAUNCHD_DAEMON_PATH..."
+
+  if [[ -f "$LAUNCHD_DAEMON_PATH" ]]; then
+    echo "launchd daemon already exists at $LAUNCHD_DAEMON_PATH. Overriding and reloading it..."
+    launchctl unload $LAUNCHD_DAEMON_PATH
+    echo "$LAUNCHD_DAEMON" > $LAUNCHD_DAEMON_PATH
+    launchctl load $LAUNCHD_DAEMON_PATH
+
+    if [[ "$SEMAPHORE_AGENT_START" == "false" ]]; then
+      echo "Not restarting $LAUNCHD_DAEMON_LABEL."
+    else
+      echo "Restarting $LAUNCHD_DAEMON_LABEL service..."
+      launchctl start $LAUNCHD_DAEMON_LABEL
+    fi
+  else
+    echo "$LAUNCHD_DAEMON" > $LAUNCHD_DAEMON_PATH
+    launchctl load $LAUNCHD_DAEMON_PATH
+
+    if [[ "$SEMAPHORE_AGENT_START" == "false" ]]; then
+      echo "Not starting $LAUNCHD_DAEMON_LABEL."
+    else
+      echo "Starting $LAUNCHD_DAEMON_LABEL service..."
+      launchctl start $LAUNCHD_DAEMON_LABEL
+    fi
+  fi
+}
+
+#
+# Main script
+#
+
+DIST=$(uname)
 AGENT_INSTALLATION_DIRECTORY="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 if [[ "$EUID" -ne 0 ]]; then
@@ -61,7 +185,15 @@ fi
 
 tar -xf toolbox.tar
 mv toolbox $TOOLBOX_DIRECTORY
-sudo chown -R $SEMAPHORE_AGENT_INSTALLATION_USER:$SEMAPHORE_AGENT_INSTALLATION_USER $TOOLBOX_DIRECTORY
+
+case $DIST in
+  Darwin)
+    sudo chown -R $SEMAPHORE_AGENT_INSTALLATION_USER $TOOLBOX_DIRECTORY
+    ;;
+  Linux)
+    sudo chown -R $SEMAPHORE_AGENT_INSTALLATION_USER:$SEMAPHORE_AGENT_INSTALLATION_USER $TOOLBOX_DIRECTORY
+    ;;
+esac
 
 sudo -u $SEMAPHORE_AGENT_INSTALLATION_USER -H bash $TOOLBOX_DIRECTORY/install-toolbox
 echo "source ~/.toolbox/toolbox" >> $USER_HOME_DIRECTORY/.bash_profile
@@ -88,57 +220,32 @@ END
 AGENT_CONFIG_PATH="$AGENT_INSTALLATION_DIRECTORY/config.yaml"
 echo "Creating agent config file at $AGENT_CONFIG_PATH..."
 echo "$AGENT_CONFIG" > $AGENT_CONFIG_PATH
-sudo chown $SEMAPHORE_AGENT_INSTALLATION_USER:$SEMAPHORE_AGENT_INSTALLATION_USER $AGENT_CONFIG_PATH
+case $DIST in
+  Darwin)
+    sudo chown $SEMAPHORE_AGENT_INSTALLATION_USER $AGENT_CONFIG_PATH
+    ;;
+  Linux)
+    sudo chown $SEMAPHORE_AGENT_INSTALLATION_USER:$SEMAPHORE_AGENT_INSTALLATION_USER $AGENT_CONFIG_PATH
+    ;;
+esac
 
 SEMAPHORE_AGENT_SYSTEMD_RESTART=${SEMAPHORE_AGENT_SYSTEMD_RESTART:-always}
 SEMAPHORE_AGENT_SYSTEMD_RESTART_SEC=${SEMAPHORE_AGENT_SYSTEMD_RESTART_SEC:-60}
 
 #
-# Create systemd service
+# Check if we can use some kind of service manager to run the agent.
+# We use systemd for Linux, and launchd for MacOS.
 #
-SYSTEMD_SERVICE=$(cat <<-END
-[Unit]
-Description=Semaphore agent
-After=network.target
-StartLimitIntervalSec=0
-
-[Service]
-Type=simple
-Restart=$SEMAPHORE_AGENT_SYSTEMD_RESTART
-RestartSec=$SEMAPHORE_AGENT_SYSTEMD_RESTART_SEC
-User=$SEMAPHORE_AGENT_INSTALLATION_USER
-WorkingDirectory=$AGENT_INSTALLATION_DIRECTORY
-ExecStart=$AGENT_INSTALLATION_DIRECTORY/agent start --config-file $AGENT_CONFIG_PATH
-
-[Install]
-WantedBy=multi-user.target
-END
-)
-
-SYSTEMD_PATH=/etc/systemd/system
-SERVICE_NAME=semaphore-agent
-SYSTEMD_SERVICE_PATH=$SYSTEMD_PATH/$SERVICE_NAME.service
-
-echo "Creating $SYSTEMD_SERVICE_PATH..."
-
-if [[ -f "$SYSTEMD_SERVICE_PATH" ]]; then
-  echo "systemd service already exists at $SYSTEMD_SERVICE_PATH. Overriding it..."
-  echo "$SYSTEMD_SERVICE" > $SYSTEMD_SERVICE_PATH
-  systemctl daemon-reload
-  if [[ "$SEMAPHORE_AGENT_START" == "false" ]]; then
-    echo "Not restarting agent."
-  else
-    echo "Restarting semaphore-agent service..."
-    systemctl restart semaphore-agent
-  fi
-else
-  echo "$SYSTEMD_SERVICE" > $SYSTEMD_SERVICE_PATH
-  if [[ "$SEMAPHORE_AGENT_START" == "false" ]]; then
-    echo "Not starting agent."
-  else
-    echo "Starting semaphore-agent service..."
-    systemctl start semaphore-agent
-  fi
-fi
+case $DIST in
+  Darwin)
+    create_launchd_daemon
+    ;;
+  Linux)
+    create_systemd_service
+    ;;
+  *)
+    echo "$DIST is not supported. You can still start the agent with '$AGENT_INSTALLATION_DIRECTORY/agent start --config $AGENT_CONFIG_PATH'."
+    ;;
+esac
 
 echo "Done."
