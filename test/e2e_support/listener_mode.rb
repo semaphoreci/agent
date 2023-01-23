4eef2d9dcc457109a9a3f9aaa90e8b64e856be77
Lucas Pinheiro <lpinheiro@renderedtext.com>
2023-01-23 15:08:28 -0300

    add kubernetes E2Es

--- test/e2e_support/listener_mode.rb
+++ test/e2e_support/listener_mode.rb
@@ -6,9 +6,15 @@ class ListenerMode
 
   def boot_up_agent
     File.write("/tmp/agent/config.yaml", $AGENT_CONFIG.to_yaml)
-    system "docker-compose -f test/e2e_support/docker-compose-listen.yml stop"
-    system "docker-compose -f test/e2e_support/docker-compose-listen.yml build"
-    system "docker-compose -f test/e2e_support/docker-compose-listen.yml up -d"
+
+    if $AGENT_CONFIG["kubernetes-executor"] do
+      system "kubectl create configmap semaphore-agent-config --from-file=semaphore-agent.yml=/tmp/agent/config.yaml"
+      system "kubectl apply -f test/e2e_support/kubernetes/deployment.yml"
+    else
+      system "docker-compose -f test/e2e_support/docker-compose-listen.yml stop"
+      system "docker-compose -f test/e2e_support/docker-compose-listen.yml build"
+      system "docker-compose -f test/e2e_support/docker-compose-listen.yml up -d"
+    end
 
     wait_for_agent_to_register_in_the_hub
   end
