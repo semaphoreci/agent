c11747298c6028d4544480a0732a7ed8056c8b54
Lucas Pinheiro <lpinheiro@renderedtext.com>
2023-02-03 16:26:14 -0300

    add integration tests

---
+++ test/e2e/kubernetes/private_image_gcr.rb
@@ -0,0 +1,85 @@
+#!/bin/ruby
+# rubocop:disable all
+
+$AGENT_CONFIG = {
+  "endpoint" => "localhost:4567",
+  "token" => "321h1l2jkh1jk42341",
+  "no-https" => true,
+  "shutdown-hook-path" => "",
+  "disconnect-after-job" => false,
+  "env-vars" => [],
+  "files" => [],
+  "fail-on-missing-files" => false,
+  "kubernetes-executor" => true,
+  "kubernetes-image-pull-policy" => "IfNotPresent"
+}
+
+require_relative '../../e2e'
+
+start_job <<-JSON
+  {
+    "id": "#{$JOB_ID}",
+
+    "executor": "dockercompose",
+
+    "compose": {
+      "containers": [
+        {
+          "name": "main",
+          "image": "#{ENV['GCR_IMAGE']}"
+        }
+      ],
+
+      "image_pull_credentials": [
+        {
+          "env_vars": [
+            { "name": "DOCKER_CREDENTIAL_TYPE", "value": "#{Base64.strict_encode64("GCR")}" },
+            { "name": "GCR_HOSTNAME", "value": "#{Base64.strict_encode64(ENV['GCR_HOSTNAME'])}" }
+          ],
+          "files": [
+            { "path": "/tmp/gcr/keyfile.json", "content": "#{ENV['GCR_KEYFILE']}", "mode": "0755" }
+          ]
+        }
+      ]
+    },
+
+    "env_vars": [],
+
+    "files": [],
+
+    "commands": [
+      { "directive": "echo Hello World" }
+    ],
+
+    "epilogue_always_commands": [],
+
+    "callbacks": {
+      "finished": "#{finished_callback_url}",
+      "teardown_finished": "#{teardown_callback_url}"
+    },
+    "logger": #{$LOGGER}
+  }
+JSON
+
+wait_for_job_to_finish
+
+assert_job_log <<-LOG
+  {"event":"job_started","timestamp":"*"}
+
+  {"event":"cmd_started",  "timestamp":"*", "directive":"Starting shell session..."}
+  *** LONG_OUTPUT ***
+  {"event":"cmd_finished", "timestamp":"*", "directive":"Starting shell session...","event":"cmd_finished","exit_code":0,"finished_at":"*","started_at":"*","timestamp":"*"}
+
+  {"directive":"Exporting environment variables","event":"cmd_started","timestamp":"*"}
+  {"directive":"Exporting environment variables","event":"cmd_finished","exit_code":0,"finished_at":"*","started_at":"*","timestamp":"*"}
+  {"directive":"Injecting Files","event":"cmd_started","timestamp":"*"}
+  {"directive":"Injecting Files","event":"cmd_finished","exit_code":0,"finished_at":"*","started_at":"*","timestamp":"*"}
+  {"directive":"echo Hello World","event":"cmd_started","timestamp":"*"}
+  {"event":"cmd_output","output":"Hello World\\n","timestamp":"*"}
+  {"directive":"echo Hello World","event":"cmd_finished","exit_code":0,"finished_at":"*","started_at":"*","timestamp":"*"}
+  {"event":"cmd_started",  "timestamp":"*", "directive":"Exporting environment variables"}
+  {"event":"cmd_output",   "timestamp":"*", "output":"Exporting SEMAPHORE_JOB_RESULT\\n"}
+  {"event":"cmd_finished", "timestamp":"*", "directive":"Exporting environment variables","exit_code":0,"started_at":"*","finished_at":"*"}
+
+  {"event":"job_finished","result":"passed","timestamp":"*"}
+LOG
