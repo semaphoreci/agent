612531087a6bf236ac8895dc532bb7d6118715c2
Lucas Pinheiro <lpinheiro@renderedtext.com>
2023-03-28 18:19:51 -0300

    fix E2Es expected job logs

--- test/e2e/kubernetes/docker_compose__file-injection.rb
+++ test/e2e/kubernetes/docker_compose__file-injection.rb
@@ -53,6 +53,9 @@ wait_for_job_to_finish
 assert_job_log <<-LOG
   {"event":"job_started",  "timestamp":"*"}
 
+  {"event":"cmd_started",  "timestamp":"*", "directive":"Creating Kubernetes resources for job..."}
+  {"event":"cmd_finished", "timestamp":"*", "directive":"Creating Kubernetes resources for job...","event":"cmd_finished","exit_code":0,"finished_at":"*","started_at":"*","timestamp":"*"}
+
   {"event":"cmd_started",  "timestamp":"*", "directive":"Starting shell session..."}
   *** LONG_OUTPUT ***
   {"event":"cmd_finished", "timestamp":"*", "directive":"Starting shell session...","event":"cmd_finished","exit_code":0,"finished_at":"*","started_at":"*","timestamp":"*"}
