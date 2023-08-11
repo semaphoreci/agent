fbf2aaf12fcc8c233c4d28e49a0e7712a5a85dc1
Lucas Pinheiro <lpinheiro@renderedtext.com>
2023-08-11 16:31:10 -0300

    remove /job_logs usage from E2Es

--- test/e2e_support/api_mode.rb
+++ test/e2e_support/api_mode.rb
@@ -51,7 +51,7 @@ class ApiMode
 
     Timeout.timeout(60 * 2) do
       loop do
-        `curl -s -H "Authorization: Bearer #{$TOKEN}" --fail -k "https://0.0.0.0:30000/job_logs" | grep "#{cmd}"`
+        `curl -s -H "Authorization: Bearer #{$TOKEN}" --fail -k "https://0.0.0.0:30000/jobs/#{$JOB_ID}/log" | grep "#{cmd}"`
 
         if $?.exitstatus == 0
           break
